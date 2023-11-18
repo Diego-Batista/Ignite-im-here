c865dfa50499ad1cb8c8ff0ec17461c1b9e6e942
Diego-Batista <diegobtistadev@gmail.com>
2023-11-18 16:27:14 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import { Alert, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Styles } from './styles';
+
+export function Home() {
+
+    function handleParticipantAdd() {
+        Alert.alert('clicou')
+    }
+
+    return (
+        <View style={Styles.container}>
+            <Text style={Styles.eventName}>Nome do evento</Text>
+            <Text style={Styles.eventDate}>Sexta, 4 de Dezembro 2023</Text>
+            
+            <View style={Styles.form}>
+                <TextInput 
+                    style={Styles.input}
+                    placeholder='Nome do participante'
+                    placeholderTextColor='#6B6B6B'
+                />
+
+                <TouchableOpacity style={Styles.button} onPress={handleParticipantAdd}>
+                    <Text style={Styles.buttonText}>+</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,47 @@
+import { StyleSheet } from 'react-native';
+
+export const Styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24,
+    },
+    eventName: {
+      color: '#FDFCFE',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 48,
+    },
+    eventDate: {
+      fontSize: 16,
+      color: '#6B6B6B'
+    },
+    form: {
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: 36,
+    },
+    input: {
+      flex: 1,
+      height: 56,
+      backgroundColor: '#1F1E25',
+      borderRadius: 5,
+      color: '#FDFCFE',
+      padding: 16,
+      fontSize: 16,
+      marginRight: 12
+    },
+    button: {
+      width: 56,
+      height: 56,
+      borderRadius: 5,
+      backgroundColor: '#31CF67',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonText: {
+      color: '#FDFCFE',
+      fontSize: 24,
+    }
+  });
